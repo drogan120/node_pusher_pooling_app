e2f74dfbfdecb5754bc4d2b83fb0cb949f4034f1
drogan120 <alimahmudin.hp@gmail.com>
2020-08-01 20:52:14 +0700

    adding view, route and pusher

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
-const expreess = require("expreess");
+const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotENV = require("dotenv");
+dotENV.config();
+
+const app = express();
+const poll = require("./app/routes/poll");
+//  Set public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// enable cors
+app.use(cors());
+
+app.use("/poll", poll);
+
+const port = process.env.APP_PORT || 3000;
+const url = process.env.APP_URL || "127.0.0.1";
+app.listen(port, () => {
+  console.log(`Server running on  http://${url}:${port}`);
+});
---
+++ app/routes/poll.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+const Pusher = require("pusher");
+let pusher = new Pusher({
+  appId: process.env.PUSHER_ID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: process.env.PUSHER_CLUSTER || "us2",
+  encrypted: process.env.PUSHER_ENCRYPTED || true,
+});
+
+router.get("/", (req, res) => {
+  res.send({ message: "pool oke" });
+});
+
+router.post("/", (req, res) => {
+  pusher.trigger("os-poll", "os-vote", {
+    point: 1,
+    os: req.body.os,
+  });
+  return res.json({ success: true, message: "Thanks for voting" });
+});
+module.exports = router;
